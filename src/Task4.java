af6e198ae5db8c9a44563887b704458b2d44c9f0
Andrii <andriy.servatiak@gmail.com>
2019-10-03 15:38:42 +0300

    пофіксив

--- src/Task4.java
+++ src/Task4.java
@@ -2,16 +2,17 @@ import java.util.Random;
 
 public class Task4 {
     public static void main(String[] args) {
-        var i = new Random().nextInt(1000);
+
         var k = 1;
         var s = 0;
         while (k <= 50) {
+            var i = new Random().nextInt(1000);
             k++;
             if (i % 7 == 0) {
                 System.out.println(i);
                 s = s + i;
             }
-            i = new Random().nextInt(1000);
+
         }
         System.out.print(s);
         System.out.print("-сума");
